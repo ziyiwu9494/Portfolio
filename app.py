3c58a1b62cd7472c28a80a950eb46fda452ba174
Ziyi <ziyiwu@usc.edu>
2020-07-15 01:26:28 -0700

    Complete design of index and projects

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['TESTING'] = True
+
+card = []
 
 
 @app.route('/')
