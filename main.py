28e38bdb06a6bc53c8a053166719da6aeb19469f
Ziyi Wu <ziyiwu@usc.edu>
2020-08-19 21:43:20 -0700

    Add card templates

--- main.py
+++ main.py
@@ -5,17 +5,16 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['TESTING'] = True
 
-card = [
-    {"title": "", "image_uri": "", "description": "", "github": "", "demo": ""},
-    {"title": "", "image_uri": "", "description": "", "github": "", "demo": ""},
-    {"title": "", "image_uri": "", "description": "", "github": "", "demo": ""},
-    {"title": "", "image_uri": "", "description": "", "github": "", "demo": ""},
+cards = [
+    {"title": "Physics", "image_uri": "https://www.dropbox.com/s/xswwcp0v0qinfu6/Blockian.png?dl=0", "description": "Physics E&M statics modeling and graphing in Python. ", "github": "https://github.com/ziyiwu9494/Physics", },
+    {"title": "Blockian", "image_uri": "https://www.dropbox.com/s/n02enkvriy3povm/Physics.png?dl=0", "description": "Square Block 3rd-person shooter created using Java Canvas", "demo": "https://www.dropbox.com/s/dg2bglvqlcr9w0q/Team431.jar?dl=0"},
+    {"title": "Breakout", "image_uri": "https://www.dropbox.com/s/nohfvrlujopx7gp/Breakout.png?dl=0", "description": "Atari breakout in Java Canvas with a couple fun new features.", "demo": "https://www.dropbox.com/s/l9saskwks2dikjv/Ziyi%20Wu%20Breakout540p.jar?dl=0"},
 ]
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Home')
+    return render_template('index.html', title='Home', cards=cards)
 
 
 if __name__ == '__main__':
